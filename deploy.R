fe6d75cc2b78802eb5f95720b69ac14c177c3096
Risa McNellis <risa.mcnellis@gmail.com>
2024-02-07 15:55:11 -0500

    Add missing library call and stop function when a variable cannot be found

--- deploy.R
+++ deploy.R
@@ -1,3 +1,15 @@
+library(rsconnect)
+
+# a function to stop the script when one of the variables cannot be found
+# and to strip quotation marks from the secrets when you supplied them
+error_on_missing_name <- function(name) {
+  var <- Sys.getenv(name, unset = NA)
+  if(is.na(var)) {
+    stop(paste0("cannot find ", name, " !"), call. = FALSE)
+  }
+  gsub("\"", "", var)
+}
+
 # Authenticate
 setAccountInfo(name='pie-lter', 
                token='C485E1BA4F0E356AF3D8430767343B01', 
