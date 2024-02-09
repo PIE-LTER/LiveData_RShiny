29fda56894bd5fc302be0f87fd91c0f77fd36010
pie_im <pie_im@mbl.edu>
2024-02-09 09:02:03 -0500

    Regular auto-commit at 09:02:02 on 02/09/2024

--- LiveData.R
+++ LiveData.R
@@ -4,6 +4,8 @@ library(lubridate)
 library(ggplot2)
 library(clifro)
 
+# setwd("C:/Users/pielter/LiveData_RShiny")
+
 ui <- fluidPage(
   titlePanel("Live Weather Data"),
   h4("Please note that this data is provisional and is not checked for quality."),
@@ -16,108 +18,149 @@ ui <- fluidPage(
                             end = now(),
                             min = now() - days(365),
                             max = now()),
-             plotOutput("tempplot", hover = "plot_hover"),
-             plotOutput("rhplot"),
-             plotOutput("precipplot"),
-    ),
-    tabPanel("Ipswich Bay Yacht Club",
-             dateRangeInput("IBdaterange", "Pick a Time Period",
-                            # value = today(),
-                            start = now() - days(7),
-                            end = now(),
-                            min = now() - days(365),
-                            max = now()),
-             plotOutput("radarplot"),
-             plotOutput("windplot"),
-             plotOutput("dirplot", width = "100%"),
+             fluidRow(
+               column(6,
+                      plotOutput("tempplot"),
+                      plotOutput("precipplot"),
+                      plotOutput("parplot"),
+                      plotOutput("wsplot")
+               ),
+               column(6,
+                      plotOutput("rhplot"),
+                      plotOutput("barplot"),
+                      plotOutput("radplot"),
+                      plotOutput("wdplot")
+               )
+             ),
+             tabPanel("Ipswich Bay Yacht Club",
+                      dateRangeInput("IBdaterange", "Pick a Time Period",
+                                     # value = today(),
+                                     start = now() - days(7),
+                                     end = now(),
+                                     min = now() - days(365),
+                                     max = now()),
+                      plotOutput("radarplot"),
+                      plotOutput("windplot"),
+                      plotOutput("dirplot", width = "100%"),
+             )
     )
-  ))
+  )
+  )
 )
-
-server <- function(input, output, session) {
-  # read files
-  IBYC_raw <- read.delim("CR1000XSeries_IBYC_raw.dat", sep = ",", header = F, skip = 4)
-  names(IBYC_raw) <- c("Timestamp",
-                       "Record",
-                       "Battery",
-                       "Temp",
-                       "Wind",
-                       "WindDir",
-                       "Wind_Max",
-                       "T_Wind_Max",
-                       "Water_Level",
-                       "Water_Level_NAVD88",
-                       "Radar_Distance",
-                       "Radar_Status")
-  IBYC_raw$Water_Level <- as.numeric(IBYC_raw$Water_Level)
-  IBYC_raw$Water_Level[IBYC_raw$Water_Level <= 0] <- NA
   
-  MFM_raw <- read.delim("CR1000XSeries_MFM_raw.dat", sep = ",", header = F, skip = 4)
-  names(MFM_raw) <- c("Timestamp",
-                      "Record",
-                      "Temp",
-                      "RH",
-                      "Wind",
-                      "Wind_Max",
-                      "T_Wind_Max",
-                      "WindDir",
-                      "Pyranometer",
-                      "Pyr_TOT",
-                      "DewPt",
-                      "PAR",
-                      "BAR",
-                      "Precip",
-                      "Battery",
-                      "PTemp")
-  MFM_raw$DewPt <- as.numeric(MFM_raw$DewPt)
+  server <- function(input, output, session) {
+    # read files
+    IBYC_raw <- read.delim("CR1000XSeries_IBYC_raw.dat", sep = ",", header = F, skip = 4)
+    names(IBYC_raw) <- c("Timestamp",
+                         "Record",
+                         "Battery",
+                         "Temp",
+                         "Wind",
+                         "WindDir",
+                         "Wind_Max",
+                         "T_Wind_Max",
+                         "Water_Level",
+                         "Water_Level_NAVD88",
+                         "Radar_Distance",
+                         "Radar_Status")
+    IBYC_raw$Water_Level <- as.numeric(IBYC_raw$Water_Level)
+    IBYC_raw$Water_Level[IBYC_raw$Water_Level <= 0] <- NA
+    
+    MFM_raw <- read.delim("CR1000XSeries_MFM_raw.dat", sep = ",", header = F, skip = 4)
+    names(MFM_raw) <- c("Timestamp",
+                        "Record",
+                        "Temp",
+                        "RH",
+                        "Wind",
+                        "Wind_Max",
+                        "T_Wind_Max",
+                        "WindDir",
+                        "Pyranometer",
+                        "Pyr_TOT",
+                        "DewPt",
+                        "PAR",
+                        "BAR",
+                        "Precip",
+                        "Battery",
+                        "PTemp")
+    MFM_raw$DewPt <- as.numeric(MFM_raw$DewPt)
+    
+    IBYC <- reactive({
+      IBYC_raw %>% 
+        filter(Timestamp >= input$IBdaterange[1] & Timestamp <= input$IBdaterange[2])
+    })  
+    MFM <- reactive({
+      MFM_raw %>% 
+        filter(Timestamp >= input$MVdaterange[1] & Timestamp <= input$MVdaterange[2])
+    })   
+    
+    output$tempplot <- renderPlot({
+      ggplot(MFM(), aes(ymd_hms(Timestamp), Temp)) + geom_line() +
+        labs(title = "Temperature", x = "Timestamp (EST)", y = "Temperature (C)") +
+        theme_classic(base_size = 20)
+    })
+    output$rhplot <- renderPlot({
+      ggplot(MFM(), aes(ymd_hms(Timestamp), RH)) + geom_line() +
+        labs(title = "Humidity", x = "Timestamp (EST)", y = "Relative Humidity (%)") +
+        theme_classic(base_size = 20)
+    })
+    output$precipplot <- renderPlot({
+      ggplot(MFM(), aes(ymd_hms(Timestamp), Precip)) + geom_line() +
+        labs(title = "Precipitation", x = "Timestamp (EST)", y = "Precipitation (mm)") +
+        theme_classic(base_size = 20)
+    })
+    output$barplot <- renderPlot({
+      ggplot(MFM(), aes(ymd_hms(Timestamp), BAR)) + geom_line() +
+        labs(title = "Barometric Pressure", x = "Timestamp (EST)", y = "Barometric Pressure (mbar)") +
+        theme_classic(base_size = 20)
+    })
+    output$parplot <- renderPlot({
+      ggplot(MFM(), aes(ymd_hms(Timestamp), PAR)) + geom_line() +
+        labs(title = "Photosynthetically Active Radiation", x = "Timestamp (EST)", y = "PAR (umol/m2/s)") +
+        theme_classic(base_size = 20)
+    })    
+    output$radplot <- renderPlot({
+      ggplot(MFM(), aes(ymd_hms(Timestamp), Pyranometer)) + geom_line() +
+        labs(title = "Shortwave Radiation", x = "Timestamp (EST)", y = "Shortwave Radiation (kW/m2)") +
+        theme_classic(base_size = 20)
+    })
+    output$wsplot <- renderPlot({
+      ggplot(MFM(), aes(ymd_hms(Timestamp), Wind)) + geom_line() +
+        labs(title = "Wind Speed", x = "Timestamp (EST)", y = "Wind Speed (m/s)") +
+        theme_classic(base_size = 20)
+    })
+    output$wdplot <- renderPlot({
+      with(MFM(), windrose(Wind, WindDir, 
+                            speed_cuts = c(2.5, 5, 7.5, 10),
+                            col_pal = "Spectral", 
+                            legend_title = "Wind Speed (m/s)")) +
+        labs(title = "Wind Direction", x = NULL, y = NULL) +
+        theme_minimal(base_size = 20)
+    })
+    output$radarplot <- renderPlot({
+      ggplot(IBYC(), aes(ymd_hms(Timestamp), Water_Level)) + geom_line() +
+        labs(title = "Water Level", x = "Timestamp (EST)", y = "Water Level (m)") +
+        theme_classic(base_size = 20)
+    })
+    output$windplot <- renderPlot({
+      ggplot(IBYC(), aes(ymd_hms(Timestamp), Wind)) + geom_line() +
+        labs(title = "Wind Speed", x = "Timestamp (EST)", y = "Wind Speed (m/s)") +
+        theme_classic(base_size = 20)
+    })
+    output$dirplot <- renderPlot({
+      with(IBYC(), windrose(Wind, WindDir, 
+                            speed_cuts = c(2.5, 5, 7.5, 10),
+                            col_pal = "Spectral", 
+                            legend_title = "Wind Speed (m/s)")) +
+        labs(title = "Wind Direction", x = NULL, y = NULL) +
+        theme_minimal(base_size = 20)
+    })
+    output$update_ts <- renderText({
+      paste("Last Updated:", last(IBYC_raw$Timestamp), "EST", sep = " ")
+    })
+  }
   
-  IBYC <- reactive({
-    IBYC_raw %>% 
-    filter(Timestamp >= input$IBdaterange[1] & Timestamp <= input$IBdaterange[2])
-  })  
-  MFM <- reactive({
-    MFM_raw %>% 
-      filter(Timestamp >= input$MVdaterange[1] & Timestamp <= input$MVdaterange[2])
-  })   
+  shinyApp(ui, server)
   
-  output$tempplot <- renderPlot({
-    ggplot(MFM(), aes(ymd_hms(Timestamp), Temp)) + geom_line() +
-      labs(title = "Temperature", x = "Timestamp (EST)", y = "Temperature (C)") +
-      theme_classic(base_size = 20)
-  })
-  output$rhplot <- renderPlot({
-    ggplot(MFM(), aes(ymd_hms(Timestamp), RH)) + geom_line() +
-      labs(title = "Humidity", x = "Timestamp (EST)", y = "Relative Humidity (%)") +
-      theme_classic(base_size = 20)
-  })
-  output$precipplot <- renderPlot({
-    ggplot(MFM(), aes(ymd_hms(Timestamp), Precip)) + geom_line() +
-      labs(title = "Precipitation", x = "Timestamp (EST)", y = "Precipitation (mm)") +
-      theme_classic(base_size = 20)
-  })
-  output$windplot <- renderPlot({
-    ggplot(IBYC(), aes(ymd_hms(Timestamp), Wind)) + geom_line() +
-      labs(title = "Wind Speed", x = "Timestamp (EST)", y = "Wind Speed (m/s)") +
-      theme_classic(base_size = 20)
-  })
-  output$radarplot <- renderPlot({
-    ggplot(IBYC(), aes(ymd_hms(Timestamp), Water_Level)) + geom_line() +
-      labs(title = "Water Level", x = "Timestamp (EST)", y = "Water Level (m)") +
-      theme_classic(base_size = 20)
-  })
-  output$dirplot <- renderPlot({
-    with(IBYC(), windrose(Wind, WindDir, 
-                          speed_cuts = c(2.5, 5, 7.5, 10),
-                          col_pal = "Spectral", 
-                          legend_title = "Wind Speed (m/s)")) +
-      labs(title = "Wind Direction", x = NULL, y = NULL) +
-      theme_minimal(base_size = 20)
-  })
-  output$update_ts <- renderText({
-    paste("Last Updated:", last(IBYC_raw$Timestamp), "EST", sep = " ")
-  })
-}
-
-shinyApp(ui, server)
-
-
+  
+  
